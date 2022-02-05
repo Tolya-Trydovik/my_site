877199448e1e056a7a0f3c51638e2c39fb2e8983
Timo <barskijtimofej@gmail.com>
2022-02-05 19:00:52 +0300

    User Admin and TODO list add
    added list TODO
    played with Admin

--- users/admin.py
+++ users/admin.py
@@ -9,6 +9,6 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username',]
+    list_display = ['email', 'username', 'phone_number']
 
 admin.site.register(CustomUser, CustomUserAdmin)
