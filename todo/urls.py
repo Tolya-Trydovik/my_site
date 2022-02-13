8eec8c3d0be8129d5076f0c211283f8e46a01144
Timothy <82282735+Tolya-Trydovik@users.noreply.github.com>
2022-02-13 16:05:08 +0200

    13.02.2022 api
    api

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,15 @@
+from rest_framework import routers
+
+from .views import TodoViewSet
+
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.render, name='render'),
-]
+router = routers.DefaultRouter()
+router.register(prefix='api/v1/todo', viewset=TodoViewSet, basename='todo')
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('', views.render, name='render'),
+# ]
